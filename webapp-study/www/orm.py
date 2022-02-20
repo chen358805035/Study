b12c0bbbadeca64eed87dc817d361ff61387b96a
chen <you@example.com>
2022-02-20 23:50:18 +0800

    daty3

--- webapp-study/www/orm.py
+++ webapp-study/www/orm.py
@@ -102,15 +102,208 @@ class Field(object):
         return "<%s, %s:%s>" % (self.__class__.__name__, self.column_type, self.name)
 
 
+# 字符类型段
 class StringField(Field):
+    # 这个函数会在对象初始化的时候调用，我们可以选择实现，也可以选择不实现，一般建议是实现的，不实现对象属性就不会被初始化。
     def __init__(self, name=None, primary_key=False, default=None, ddl="varchar(100)"):
         # StringField__init__为父类调用Field__init__；self.__class__.__name__为StringField
         super().__init__(name, ddl, primary_key, default)
 
 
+# 布尔类型段
 class BooleanField(Field):
     def __init__(self, name=None, default=False):
         super().__init__(name, "boolean", False, default)
+
+
+# 整形类型段
+class IntegerField(Field):
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, "bigint", primary_key, default)
+
+
+# 浮点数类型段
+class FloatField(Field):
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, "real", primary_key, default)
+
+
+# 文本类型段
+class TextField(Field):
+    def __init__(self, name=None, default=None):
+        super().__init__(name, "text", False, default)
+
+
+# 映射信息读取
+class ModelMetaclass(type):
+    # __new__() 是一种负责创建类实例的静态方法，它无需使用 staticmethod 装饰器修饰，且该方法会优先 __init__() 初始化方法被调用。
+    def __new__(cls, name, bases, attrs):
+        if name == "Model":
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get("__table__", None) or name
+        logging.info("找到模型: %s (表格： %s" % (name, tableName))
+        # 映射数据定义为字典
+        mappings = dict()
+        fields = []
+        # 关键字
+        primaryKey = None
+        # 获取字典键值和值
+        for k, v in attrs.items():
+            # isinstance() 函数来判断一个对象是否是一个已知的类型，类似 type()。
+            if isinstance(v, Field):
+                logging.info("找到映射： %s ==> %s" % (k, v))
+                mappings[k] = v
+                # 如果键值不为空
+                if v.primary_key:
+                    # 找到主键值。如果primaryKey不为空则执行错误
+                    if primaryKey:
+                        raise StandardError("复制字段的主键: %s" % k)
+                    primaryKey = v
+                # 如果键值为空则将值添加到fields中
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise StandardError("主键未找到.")
+        # 获取映射数据中的键值移除
+        for k in mappings.keys():
+            # pop() 函数用于移除列表中的一个元素（默认最后一个元素），并且返回该元素的值。
+            # 移除attrs中对应映射数据中的键值
+            attrs.pop(k)
+        # lambda f: '`%s`' % f相当于def(f) return '`%s`' % f
+        # ap(lambda f: '`%s`' % f, fields)相当于把fields中的数据变成'x','x'...
+        escaped_fields = list(map(lambda f: "`%s`" % f, fields))
+        # print(escaped_fields)
+        attrs["__mappings__"] = mappings  # 保存属性和列的映射关系
+        attrs["__table__"] = tableName
+        attrs["__primary_key__"] = primaryKey  # 主键属性名
+        attrs["__fields__"] = fields  # 除主键外的属性名
+
+        # 构造默认的SELECT, INSERT, UPDATE和DELETE语句:
+        attrs["__select__"] = "select `%s`, %s from `%s`" % (
+            primaryKey,
+            ", ".join(escaped_fields),
+            tableName,
+        )
+        attrs["__insert__"] = "insert into `%s` (%s, `%s`) values (%s)" % (
+            tableName,
+            ", ".join(escaped_fields),
+            primaryKey,
+            create_args_string(len(escaped_fields) + 1),
+        )
+        attrs["__update__"] = "update `%s` set %s where `%s`=?" % (
+            tableName,
+            ", ".join(map(lambda f: "`%s`=?" % (mappings.get(f).name or f), fields)),
+            primaryKey,
+        )
+        attrs["__delete__"] = "delete from `%s` where `%s`=?" % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+
+# 定义模板类metaclass元素为ModelMetaclass
+class Model(dict, metaclass=ModelMetaclass):
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        # 获取不存在的属性将会抛出下面的异常
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' 对象没有属性。 '%s'" % key)
+
+    # 在类实例的每个属性进行赋值时，都会首先调用__setattr__()方法，并在__setattr__()方法中将属性名和属性值添加到类实例的__dict__属性中
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        # print(type(key))
+        value = getattr(self, str(key), None)
+        print(type(key))
+        print(value)
+        # 如果值为空
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                # callable() 函数用于检查一个对象是否是可调用的。如果返回 True，object 仍然可能调用失败；但如果返回 False，调用对象 object 绝对不会成功
+                value = field.default() if callable(field.default) else field.default
+                logging.debug("使用的默认值 %s: %s" % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    # 查找所有
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        #' 通过where子句查找对象. '
+        sql = [cls.__select__]
+        if where:
+            sql.append("where")
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get("orderBy", None)
+        if orderBy:
+            sql.append("order by")
+            sql.append(orderBy)
+        limit = kw.get("limit", None)
+        if limit is not None:
+            sql.append("limit")
+            if isinstance(limit, int):
+                sql.append("?")
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append("?, ?")
+                # extend() 函数用于在列表末尾一次性追加另一个序列中的多个值（用新列表扩展原来的列表）。
+                args.extend(limit)
+            else:
+                raise ValueError("无效的极限值: %s" % str(limit))
+        rs = await select(" ".join(sql), args)
+        return [cls(**r) for r in rs]
+
+    # 查找对应的数据
+    @classmethod
+    async def findNumber(cls, selectField, where=None, args=None):
+        #' 通过select和where查找数字. '
+        sql = ["select %s _num_ from `%s`" % (selectField, cls.__table__)]
+        if where:
+            sql.append("where")
+            sql.append(where)
+        rs = await select(" ".join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]["_num_"]
+
+    @classmethod
+    async def find(cls, pk):
+        #' 通过主键查找对象. '
+        rs = await select(
+            "%s where `%s`=?" % (cls.__select__, cls.__primary_key__), [pk], 1
+        )
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn("插入记录失败: affected rows: %s" % rows)
+
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warn("通过主键更新失败: 受影响的行: %s" % rows)
+
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warn("按主键删除失败: 受影响的行: %s" % rows)
 
 
 # a = StringField()
--- webapp-study/www/test.py
+++ webapp-study/www/test.py
@@ -1,7 +1,29 @@
-d = {"Michael": 95, "Bob": 75, "Tracy": 85}
-b = [d.get("Michael"), d.get("Bod"), d.get("Tracy")]
+# d = {"Michael": 95, "Bod": 75, "Tracy": 85}
+# b = [d.get("Michael"), d.get("Bod"), d.get("Tracy")]
+# a = dict()
+# # # a = d.get("Michael")
+# # for k, v in d.items():
+# #     print("key = %s,value = %s" % (k, v))
+# # # with d.get() as a:
+# # print(a)
 
-# a = d.get("Michael")
-print(b.get())
-# with d.get() as a:
-#   print(a)
+# # print(b)
+# # print(list(map(lambda f: "`%s`" % f, b)))
+
+import orm, asyncio
+from models import User, Blog, Comment
+
+
+async def test(loop):
+    await orm.create_pool(loop=loop, user="cb", password="cb", db="cb")
+
+    u = User(
+        name="Test", email="test@example.com", passwd="1234567890", image="about:blank"
+    )
+
+    await u.save()
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
